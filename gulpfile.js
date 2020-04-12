d71b514572ca10b18d63cafcc9d0ac854f74a912
AmeerAnsari <hawkthunder47@gmail.com>
2020-04-12 12:45:42 +0500

    adding the css watch funcition

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,16 @@ gulp.task('html', function() {
     console.log('the html task');
 });
 
+gulp.task('css', function() {
+    console.log('Imagine Sass or post css task runing here.');
+});
+
 gulp.task('watch', function() {
 
    // we use the gulp series insted of 'start function'
    watch('./app/index.html', gulp.series('html'));
+   watch('./app/assets/images/styles/**/*.css', gulp.series('css'));
+
 });
+
+
